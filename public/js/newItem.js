d7ac151e131f1c229d5fabc2f3cd03b388c1e758
Brooksteven <Brooksteven580@gmail.com>
2023-02-15 15:32:14 -0500

    updated files

---
+++ public/js/newItem.js
@@ -0,0 +1,47 @@
+const saveNewItemHandler = async (event) => {
+    event.preventDefault();
+    // Collect values from the /newGift form
+    const recipientName = document.querySelector("#newitemItemee").value.trim();
+    const description = document
+      .querySelector("#newitemDescription")
+      .value.trim();
+  
+    // recipientName and description are required to create a new Gift
+    if (recipientName && description) {
+      // Send a POST request to the API endpoint
+      const response = await fetch("/api/items", {
+        method: "POST",
+        body: JSON.stringify({ recipientName, description }),
+        headers: { "Content-Type": "application/json" },
+      });
+  
+      if (response.ok) {
+        // If successful, redirect the browser to the profile page
+        document.location.replace("/userlanding");
+      } else {
+        alert(response.statusText);
+      }
+    }
+  
+    const response = await fetch(`/api/items/${id}`, {
+      method: "POST",
+    });
+  
+    if (response.ok) {
+      document.location.replace("/userlanding");
+    } else {
+      alert('Failed to add project');
+    }
+  };
+  
+  const cancelButtonHandler = async () => {
+    document.location.replace('/userlanding');
+  }
+  
+  document
+    .querySelector('.newItemForm')
+    .addEventListener('reset', cancelButtonHandler);
+  
+  document
+    .querySelector(".newItemForm")
+    .addEventListener("submit", saveNewItemHandler);
